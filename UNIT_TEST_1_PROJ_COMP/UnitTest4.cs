cf7632206fd9a36d4b6ca3bcf330832ffc754e14
TheNisham <77533996+nishamghimire5@users.noreply.github.com>
2024-06-08 21:46:18 +0545

    reuploading due to errors in prev repo

---
+++ UNIT_TEST_1_PROJ_COMP/UnitTest4.cs
@@ -0,0 +1,48 @@
+﻿using PROJ_COMP;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest1_Assignment
+{
+    [TestClass]
+    public class Method_tests
+    {
+        Form1 fm = new Form1();
+        /// <summary>
+        /// testing for methods
+        /// </summary>
+        [TestMethod]
+        public void check_method_command()
+        {
+            string[] method_text = { "method test ()\ncircle (100)\nendmethod", "method test1 (radius, width, height)" +
+                    "\ncircle (radius)\nrectangle (width, height)\nendmethod" };
+            string method_line = "";
+            int method_found_in = 1;
+            for (int i = 0; i < method_text.Length; i++)
+            {
+                if (i == 0)
+                {
+                    //without parameter
+                    method_line = "method test ()";
+                }
+                else
+                {
+                    //with parameter
+                    method_line = "method test1 (radius, width, height)";
+                }
+                string[] commands = method_text[i].Split('\n');
+                Assert.IsTrue(CommandParser.GetInstance.check_method(method_line));
+                Assert.IsTrue(Loop.GetInstance.run_method_command(method_line, commands, method_found_in, fm));
+            }
+
+            //method call
+            string[] method_call = { "test ()", "test1 (10,20,30)" };
+            foreach (string cmd in method_call)
+            {
+                Assert.IsTrue(CommandParser.GetInstance.check_methodcall(cmd));
+                Assert.IsTrue(Loop.GetInstance.run_method_call(cmd, fm));
+            }
+
+        }
+    }
+    }
