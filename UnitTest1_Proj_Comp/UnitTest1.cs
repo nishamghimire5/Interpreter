200dac2ce8540fe5488d63f81b9168a7e07bcad2
TheNisham <77533996+nishamghimire5@users.noreply.github.com>
2024-06-13 10:26:12 +0545

    UnitTest has no issues

---
+++ UnitTest1_Proj_Comp/UnitTest1.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PROJ_COMP;
+using System;
+
+namespace UnitTest1_PROJ_COMP
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestClass]
+        public class Drawing_commands_test
+        {
+            /// <summary>
+            /// Check validitiy of executing commands
+            /// </summary>
+            [TestMethod]
+            public void check_executing_command()
+            {
+                string[] commands = { "run", "clear", "reset" };
+                foreach (string cmd in commands)
+                {
+                    Assert.IsTrue(CommandParser.GetInstance.Valid_execute_command(cmd));
+                }
+            }
+
+            /// <summary>
+            /// Check moveto command validity
+            /// </summary>
+            [TestMethod]
+            public void check_moveto_command()
+            {
+                //valid command with valid parameters
+                Assert.IsTrue(CommandParser.GetInstance.run_command("moveto(20,30)"));
+                //invalid parameter
+                Assert.IsFalse(CommandParser.GetInstance.run_command("moveto(20,y)"));
+                //invalid command name
+                Assert.IsFalse(CommandParser.GetInstance.run_command("movto(20,30)"));
+            }
+
+            /// <summary>
+            /// Check drawto command validity
+            /// </summary>
+            [TestMethod]
+            public void check_drawto_command()
+            {
+                //valid command with valid parameters
+                Assert.IsTrue(CommandParser.GetInstance.run_command("drawto(20,30)"));
+                //invalid parameter
+                Assert.IsFalse(CommandParser.GetInstance.run_command("drawto(20,y)"));
+                //invalid command name
+                Assert.IsFalse(CommandParser.GetInstance.run_command("drwtos(20,30)"));
+            }
+
+            /// <summary>
+            /// Check fill command validity
+            /// </summary>
+            [TestMethod]
+            public void check_fill_command()
+            {
+                //valid command with valid parameter
+                Assert.IsTrue(CommandParser.GetInstance.run_command("fill(on)"));
+                //invalid parameter
+                Assert.IsFalse(CommandParser.GetInstance.run_command("fill(on,off)"));
+                //invalid command name
+                Assert.IsFalse(CommandParser.GetInstance.run_command("filsl(on)"));
+
+            }
+
+            /// <summary>
+            /// Check pen command validity
+            /// </summary>
+            [TestMethod]
+            public void check_pen_command()
+            {
+                //valid command with valid parameter
+                Assert.IsTrue(CommandParser.GetInstance.run_command("pen(red)"));
+                //invalid parameter
+                Assert.IsFalse(CommandParser.GetInstance.run_command("pen(nocolor)"));
+                //invalid command name
+                Assert.IsFalse(CommandParser.GetInstance.run_command("pn(red)"));
+            }
+
+            /// <summary>
+            /// Check rectangle shape command validity
+            /// </summary>
+            [TestMethod]
+            public void check_rectangle_command()
+            {
+                //valid command with valid parameter
+                Assert.IsTrue(CommandParser.GetInstance.run_command("rectangle(20,30)"));
+                //invalid parameter
+                Assert.IsFalse(CommandParser.GetInstance.run_command("rectangle(2t,30,40)"));
+                //invalid command name
+                Assert.IsFalse(CommandParser.GetInstance.run_command("rectanglesc(20,30)"));
+            }
+
+            /// <summary>
+            /// Check triangle shape command validity
+            /// </summary>
+            [TestMethod]
+            public void check_triangle_command()
+            {
+                //valid command with valid parameter
+                Assert.IsTrue(CommandParser.GetInstance.run_command("triangle(20,40,50)"));
+                //invalid parameter
+                Assert.IsFalse(CommandParser.GetInstance.run_command("triangle(40,50)"));
+                //invalid command name
+                Assert.IsFalse(CommandParser.GetInstance.run_command("traing(20,40,50)"));
+
+            }
+
+            /// <summary>
+            /// Check Polygon shape command validity
+            /// </summary>
+            [TestMethod]
+            public void check_polygon_command()
+            {
+                //valid command with valid parameter
+                Assert.IsTrue(CommandParser.GetInstance.run_command("polygon(20,40,50,50)"));
+                //Invalid parameter length
+                Assert.IsFalse(CommandParser.GetInstance.run_command("polygon(40,50)"));
+                //invalid command name
+                Assert.IsFalse(CommandParser.GetInstance.run_command("polgon(20,40,50)"));
+            }
+
+            /// <summary>
+            /// Check Rotate command validity
+            /// </summary>
+            [TestMethod]
+            public void check_rotate_command()
+            {
+                //valid command with valid parameter
+                Assert.IsTrue(CommandParser.GetInstance.run_command("rotate(20)"));
+                //Invalid parameter
+                Assert.IsFalse(CommandParser.GetInstance.run_command("rotate(xy, 290)"));
+                //Invalid command name
+                Assert.IsFalse(CommandParser.GetInstance.run_command("rotae(120)"));
+            }
+
+            /// <summary>
+            /// Check Circle shape command validity
+            /// </summary>
+            [TestMethod]
+            public void check_circle_command()
+            {
+                //valid command with valid parameter
+                Assert.IsTrue(CommandParser.GetInstance.run_command("circle(20)"));
+                //Invalid parameter
+                Assert.IsFalse(CommandParser.GetInstance.run_command("circle(20,3x)"));
+                //Invalid command name
+                Assert.IsFalse(CommandParser.GetInstance.run_command("circ(20)"));
+
+            }
+
+
+
+        }
+    }
+}
---
+++ UnitTest1_Proj_Comp/UnitTest5.cs
@@ -0,0 +1,33 @@
+﻿using PROJ_COMP;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest1_PROJ_COMP
+{
+    [TestClass]
+    public class Variable_test
+    {
+        Form1 fm = new Form1();
+
+        /// <summary>
+        /// test for the variables
+        /// </summary>
+        [TestMethod]
+        public void Variable_command_and_operations_test()
+        {
+            string[] text = { "width = 10", "rectangle=50", "height= 5" };
+            foreach (string check_text in text)
+            {
+                Assert.IsTrue(CommandParser.GetInstance.check_variable(check_text));
+                Assert.IsTrue(Loop.GetInstance.run_variable_command(check_text));
+
+            }
+            string[] operation_text = { "width+10", "rectangle-20", "height*5", "height/5" };
+            foreach (string check_text in operation_text)
+            {
+                Assert.IsTrue(CommandParser.GetInstance.check_variable_operation(check_text));
+                Assert.IsTrue(Loop.GetInstance.runVariableOperation(check_text, fm));
+            }
+        }
+    }
+    }
